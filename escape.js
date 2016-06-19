bf6e8a2ee8c8efd285a10e231c1e4b2f9a7b9c9b
Tu Tv <tutv95@gmail.com>
2016-06-20 01:50:11 +0700

    Updated bot

---
+++ escape.js
@@ -0,0 +1,18 @@
+function padWithLeadingZeros(string) {
+    return new Array(5 - string.length).join("0") + string;
+}
+
+function unicodeCharEscape(charCode) {
+    return "\\u" + padWithLeadingZeros(charCode.toString(16));
+}
+
+function unicodeEscape(string) {
+    return string.split('')
+        .map(function (char) {
+            var charCode = char.charCodeAt(0);
+            return charCode > 127 ? unicodeCharEscape(charCode) : char;
+        })
+        .join('');
+}
+
+module.exports.unicodeEscape = unicodeEscape();
